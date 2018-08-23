72c865dec02d2aa207d2571a1de040f150399f1c
Brendan Kerrigan <kerriganb@ainfosec.com>
2018-08-23 13:51:55 -0400

    Debug hacks.
    Signed-off-by: Brendan Kerrigan <kerriganb@ainfosec.com>

--- drivers/gpu/drm/i915/gvt/dmabuf.c
+++ drivers/gpu/drm/i915/gvt/dmabuf.c
@@ -63,7 +63,10 @@ static int vgpu_gem_get_pages(
 	}
 	gtt_entries = (gen8_pte_t __iomem *)dev_priv->ggtt.gsm +
 		(fb_info->start >> PAGE_SHIFT);
+
 	for_each_sg(st->sgl, sg, page_num, i) {
+                gvt_vgpu_err("ggtt.gsm: %p -- gtt_entries: %p\n", (gen8_pte_t __iomem *)dev_priv->ggtt.gsm, gtt_entries);
+                gvt_vgpu_err("[%d] -- GTT_ENTRY_PTR: pte addr: [%p]%p\n", i, &gtt_entries[i], gtt_entries[i]);
 		sg->offset = 0;
 		sg->length = PAGE_SIZE;
 		sg_dma_address(sg) =
